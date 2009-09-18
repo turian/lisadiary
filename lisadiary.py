a3f3a2266a5399d1c06a6453ac8451b04012dca3
Joseph Turian <turian@gmail.com>
2009-09-17 20:23:56 -0400

    Compute file last changed value

--- lisadiary.py
+++ lisadiary.py
@@ -14,6 +14,8 @@ ISSUES:
  * I walk the entire repository graph. This might be slow.
 """
 
+from common.mydict import sort as dictsort
+
 import mercurial.error
 from mercurial import hg,ui
 #uio = ui.ui(debug=True,verbose=True)
@@ -24,7 +26,7 @@ import logging
 #logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG,)
 logging.basicConfig(level=logging.DEBUG,)
 
-filechanges = {}
+filelastchanged = {}
 
 # Walk every commit
 ctxdone = {}
@@ -34,17 +36,27 @@ while len(ctxtodo) > 0:
     if ctx in ctxdone: continue
     logging.debug("Working on ctx %s" % `ctx`)
 
+    # Look at every file in this commit
     for f in ctx.files():
         logging.debug("Looking at file %s" % f)
         try:
             fctx = ctx.filectx(f)
             assert fctx.path() == f
-            print fctx
-            print fctx, fctx.date()
+            if f not in filelastchanged: filelastchanged[f] = fctx.date()[0]
+
+            # ISSUE: I'm not sure if it's correct to grab field 0. I don't know what field 1 does.
+            if filelastchanged[f] < fctx.date()[0]:
+                logging.debug("Updating last changed date of file %s from %f to %f" % (f, filelastchanged[f], fctx.date()[0]))
+                filelastchanged[f] = fctx.date()[0]
         except mercurial.error.LookupError:
+            # ISSUE: I have no idea why this exception occurs.
             logging.error("mercurial.error.LookupError on file %s at revision %s" % (ctx, f))
 
     ctxdone[ctx] = True
     for newctx in ctx.parents():
         if newctx in ctxdone: continue
         ctxtodo.append(newctx)
+
+#for f in filelastchanged:
+#    print f, filelastchanged[f]
+print dictsort(filelastchanged)
